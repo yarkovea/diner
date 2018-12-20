326aadd749295d85934f00ea4ed5d5173239376a
yarkovea <prpg@mail.ru>
2018-12-20 13:59:21 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-echo "hello world";
-echo 'изменения в github';
-$s1 = 'переменная 1';
-$s2 = "переменная 2";
+    echo "hello world";
+    echo 'изменения в github';
+    $s1 = 'переменная 1';
+    $s2 = "переменная 2";
 
-echo "$s1 $s2";
+    echo "$s1 $s2";
+    print_r($s2);
 ?>
